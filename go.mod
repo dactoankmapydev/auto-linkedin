45f77d5cd8b33387e3136b2c6e403ab21fc6d5ea
dactoankmapydev <nguyendactoankma@gmail.com>
2022-01-03 01:53:53 +0700

    test login linkedin, submit google

--- go.mod
+++ go.mod
@@ -1,9 +1,19 @@
 module auto-linkedin
 
-go 1.16
+go 1.17
 
 require (
 	github.com/chromedp/cdproto v0.0.0-20210625233425-810000e4a4fc // indirect
 	github.com/chromedp/chromedp v0.7.3
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
+)
+
+require (
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0-rc.5 // indirect
+	github.com/joho/godotenv v1.4.0
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,37 +2,68 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/chromedp/chromedp"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	// tạo phiên bản chrome
-	ctx, cancel := chromedp.NewContext(
-		context.Background(),
-		chromedp.WithLogf(log.Printf),
-	)
-	defer cancel()
-
-	// tạo thời gian chờ
-	ctx, cancel = context.WithTimeout(ctx, 15*time.Second)
-	defer cancel()
-
-	// điều hướng đến một trang, đợi một phần tử, nhấp vào
-	var example string
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(`https://golang.org/pkg/time/`),
-		// đợi phần tử chân trang hiển thị (tức là trang được tải)
-		chromedp.WaitVisible(`body > footer`),
-		// tìm và nhấp vào liên kết "Expand All"
-		chromedp.Click(`#pkg-examples > div`, chromedp.NodeVisible),
-		// lấy giá trị của textarea
-		chromedp.Value(`#example_After .play .input textarea`, &example),
-	)
-	if err != nil {
-		log.Fatal(err)
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		return
 	}
-	log.Printf("Go's time.After example:\n%s", example)
+}
+
+func main() {
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", false),
+		chromedp.Flag("disable-gpu", false),
+		chromedp.Flag("enable-automation", false),
+		chromedp.Flag("disable-extensions", false),
+	)
+	allocCtx, cancel := chromedp.NewExecAllocator(
+		context.Background(),
+		opts...,
+	)
+	defer cancel()
+
+	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	errTaskLinkedinLogin := chromedp.Run(ctx, LinkedinLogin())
+	if errTaskLinkedinLogin != nil {
+		log.Fatal(errTaskLinkedinLogin)
+	}
+
+	skill := "php"
+	location := "hanoi"
+	errTaskSearchProfile := chromedp.Run(ctx, SearchProfile(skill, location))
+	if errTaskSearchProfile != nil {
+		log.Fatal(errTaskSearchProfile)
+	}
+}
+
+func LinkedinLogin() chromedp.Tasks {
+	url := "https://www.linkedin.com"
+	emailField := "//*[@id='session_key']"
+	passwordField := "//*[@id='session_password']"
+	submitButton := "//*[@id='main-content']/section[1]/div/div/form/button"
+
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.SendKeys(emailField, ""),
+		chromedp.SendKeys(passwordField, ""),
+		chromedp.Submit(submitButton),
+		chromedp.Sleep(3 * time.Second),
+	}
+}
+
+func SearchProfile(skill, location string) chromedp.Tasks {
+	url := fmt.Sprintf("https://www.google.com/search?q=site:linkedin.com/in/ AND '%s' AND '%s'", skill, location)
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.Sleep(3 * time.Second),
+	}
 }
